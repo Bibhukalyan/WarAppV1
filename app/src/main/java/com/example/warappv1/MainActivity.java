fdd7a255b112e590b4d7269bfe8575ad6f396124
Admin <ojhabibhukalyan0@gmail.com>
2019-11-18 17:19:25 +0530

    initial commit

---
+++ app/src/main/java/com/example/warappv1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.warappv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.warappv1.fragment.ButtonListFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.frameForFragment,new ButtonListFragment())
+        .commit();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
